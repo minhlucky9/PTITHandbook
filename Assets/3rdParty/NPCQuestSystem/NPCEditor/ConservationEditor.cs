e6533db5ffd4d1b30d54356a16acf4cbcd94c9f3
VUTHANHPHONGs <155960107+VUTHANHPHONGs@users.noreply.github.com>
2025-02-27 17:24:42 +0700

    My local changes

--- Assets/3rdParty/NPCQuestSystem/NPCEditor/ConservationEditor.cs
+++ Assets/3rdParty/NPCQuestSystem/NPCEditor/ConservationEditor.cs
@@ -1,208 +1,258 @@
-using Interaction;
+
+﻿using Interaction;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-#if UNITY_EDITOR
+
 
 public class ConservationEditor : NodeEditor
 {
-	public static NPCConservationSO modifyConservation;
+    public static NPCConservationSO modifyConservation;
 
-	bool creatingAnswer = false;
-	Rect temporaryAnswer;
-	bool blocking = false;
+    bool creatingAnswer = false;
+    Rect temporaryAnswer;
+    bool blocking = false;
+
+
+
+
+
 
     private void OnEnable()
     {
-		EditorUtility.SetDirty(modifyConservation);
+        EditorUtility.SetDirty(modifyConservation);
     }
 
     private void OnDisable()
     {
-		EditorUtility.ClearDirty(modifyConservation);
+        if (modifyConservation != null)
+        {
+            EditorUtility.SetDirty(modifyConservation);
+            AssetDatabase.SaveAssets(); // Lưu dữ liệu vào file
+        }
+        EditorUtility.ClearDirty(modifyConservation);
     }
 
+
     public static void ShowEditor(NPCConservationSO conservation)
-	{
-		modifyConservation = conservation;
-		ConservationEditor editor = GetWindow<ConservationEditor>(conservation.name);
-		editor.Init();
-	}
-
-	public override void Init()
-	{
-		base.Init();
-		SetupNodeData();
-	}
-
-	private void SetupNodeData()
     {
-		nodes.Clear();
-		for (int i = 0; i < modifyConservation.dialogs.Count; i++)
-		{
-			Vector2 nodePos = modifyConservation.dialogs[i].nodePosition;
-			nodes.Add(new Rect(nodePos.x, nodePos.y, 200, 100));
-		}
-		names = GetDialogIds();
-		blocking = false;
-	}
+        modifyConservation = conservation;
+        ConservationEditor editor = GetWindow<ConservationEditor>(conservation.name);
+        editor.Init();
+    }
+
+    public override void Init()
+    {
+        base.Init();
+        SetupNodeData();
+    }
+
+    private void SetupNodeData()
+    {
+        nodes.Clear();
+        for (int i = 0; i < modifyConservation.dialogs.Count; i++)
+        {
+            Vector2 nodePos = modifyConservation.dialogs[i].nodePosition;
+            nodes.Add(new Rect(nodePos.x, nodePos.y, 200, 100));
+        }
+        names = GetDialogIds();
+        blocking = false;
+    }
 
     public override void OnGUI()
     {
-		if (blocking) return;
+        if (blocking) return;
 
         base.OnGUI();
-		//
-		switch (Event.current.keyCode)
-		{
-			case KeyCode.Delete:
-				// ...
-				DeleteDialog(selectedNode);
-				return;
-			default:
-				break;
-		}
-	}
+        //
+        switch (Event.current.keyCode)
+        {
+            case KeyCode.Delete:
+                // ...
+                DeleteDialog(selectedNode);
+                return;
+            default:
+                break;
+        }
+    }
 
     public override void CreateMenu(ref GenericMenu menu, Vector2 mousePosition)
     {
-		menu.AddItem(new GUIContent("Dialog"), false, () => OnCreateDialog(mousePosition));
-		menu.AddItem(new GUIContent("Answer"), false, () => OnCreateAnswer(mousePosition));
-	}
+        menu.AddItem(new GUIContent("Dialog"), false, () => OnCreateDialog(mousePosition));
+        menu.AddItem(new GUIContent("Answer"), false, () => OnCreateAnswer(mousePosition));
+    }
 
-	private void OnCreateAnswer(Vector2 mousePosition)
+    private void OnCreateAnswer(Vector2 mousePosition)
     {
-		creatingAnswer = true;
-		temporaryAnswer = new Rect(mousePosition.x, mousePosition.y, 200, 100);
-		//enable line
-		isConnect = true;
-		selectedRect = temporaryAnswer;
+        creatingAnswer = true;
+        temporaryAnswer = new Rect(mousePosition.x, mousePosition.y, 200, 100);
+        //enable line
+        isConnect = true;
+        selectedRect = temporaryAnswer;
 
-	}
+    }
 
-	private void OnCreateDialog(Vector2 mousePosition)
-	{
-		DialogConservation dialog = new DialogConservation();
-		dialog.dialogId = GetNotCreatedId();
-		dialog.nodePosition = mousePosition;
-		//
-		modifyConservation.dialogs.Add(dialog);
-		//
-		SetupNodeData();
-	}
+    private void OnCreateDialog(Vector2 mousePosition)
+    {
+        Interaction.DialogConservation dialog = new Interaction.DialogConservation();
+
+        dialog.dialogId = GetNotCreatedId();
+        dialog.nodePosition = mousePosition;
+        //
+        modifyConservation.dialogs.Add(dialog);
+        //
+        SetupNodeData();
+    }
 
     public override void DrawNodes()
     {
         base.DrawNodes();
-		//
-		if(creatingAnswer)
+        //
+        if (creatingAnswer)
         {
-			temporaryAnswer = GUILayout.Window(nodes.Count + 1, temporaryAnswer, DrawAnswer, "Answer");
-		}
-		
-	}
+            temporaryAnswer = GUILayout.Window(nodes.Count + 1, temporaryAnswer, DrawAnswer, "Answer");
+        }
 
-	void DrawAnswer(int id)
+    }
+
+    void DrawAnswer(int id)
     {
         Event currentEvent = Event.current;
         if (currentEvent.type == EventType.MouseDown)
         {
-			HideTemporaryAnswer();
-		}
+            HideTemporaryAnswer();
+        }
         //
         EditorGUILayout.Separator();
-		EditorStyles.label.wordWrap = true;
-		EditorGUILayout.LabelField(" ");
-		EditorStyles.label.wordWrap = false;
-		//
-		GUI.DragWindow();
-	}
+        EditorStyles.label.wordWrap = true;
+        EditorGUILayout.LabelField(" ");
+        EditorStyles.label.wordWrap = false;
+        //
+        GUI.DragWindow();
+    }
 
-	void HideTemporaryAnswer()
+    void HideTemporaryAnswer()
     {
-		creatingAnswer = false;
-		isConnect = false;
-		Repaint();
-	}
+        creatingAnswer = false;
+        isConnect = false;
+        Repaint();
+    }
 
     public override void DrawNodeContent(int i)
     {
-		if (i >= modifyConservation.dialogs.Count) return;
-		EditorGUILayout.Separator();
-		EditorStyles.label.wordWrap = true;
-		EditorGUILayout.LabelField(modifyConservation.dialogs[i].message);
-		EditorStyles.label.wordWrap = false;
-	}
+        if (i >= modifyConservation.dialogs.Count) return;
 
-	public override void OnDragNode(int i)
+        EditorGUILayout.Separator();
+
+        // GUIStyle cho TextArea
+        GUIStyle textAreaStyle = new GUIStyle(EditorStyles.textArea);
+        textAreaStyle.wordWrap = true; // Cho phép tự động xuống dòng
+
+        // Lấy nội dung hiện tại của dialog
+        string oldMessage = modifyConservation.dialogs[i].message;
+
+        // Tính chiều rộng tối đa hợp lý (giới hạn tối đa 400px)
+        float maxWidth = 800;
+        float minWidth = 400;
+        float contentWidth = Mathf.Clamp(textAreaStyle.CalcSize(new GUIContent(oldMessage)).x + 20, minWidth, maxWidth);
+
+        // Tính chiều cao dựa trên nội dung với chiều rộng tính toán được
+        float textHeight = textAreaStyle.CalcHeight(new GUIContent(oldMessage), contentWidth);
+
+        // Hiển thị TextArea với kích thước động
+        string newMessage = EditorGUILayout.TextArea(oldMessage, textAreaStyle,
+            GUILayout.Width(contentWidth), GUILayout.Height(textHeight));
+
+        // Nếu nội dung thay đổi, cập nhật dữ liệu
+        if (newMessage != oldMessage)
+        {
+            modifyConservation.dialogs[i].message = newMessage;
+            EditorUtility.SetDirty(modifyConservation);
+        }
+
+        // Cập nhật kích thước node dựa trên nội dung
+        nodes[i] = new Rect(nodes[i].x, nodes[i].y, contentWidth, Mathf.Max(textHeight + 20, 40));
+    }
+
+
+
+
+    public override void OnDragNode(int i)
     {
-		if (i >= modifyConservation.dialogs.Count) return;
+        if (i >= modifyConservation.dialogs.Count) return;
 
         base.OnDragNode(i);
-		modifyConservation.dialogs[i].nodePosition = new Vector2(nodes[i].x, nodes[i].y);
-	}
+        modifyConservation.dialogs[i].nodePosition = new Vector2(nodes[i].x, nodes[i].y);
+    }
 
     public override void OnMouseDownNode(int id)
     {
         base.OnMouseDownNode(id);
-		HideTemporaryAnswer();
-	}
+        HideTemporaryAnswer();
+    }
 
     public override void OnMouseDownMenu()
     {
         base.OnMouseDownMenu();
-		HideTemporaryAnswer();
-	}
+        HideTemporaryAnswer();
+    }
 
     private string[] GetDialogIds()
     {
-		string[] ids = new string[modifyConservation.dialogs.Count];
-		for(int i = 0; i < modifyConservation.dialogs.Count; i ++)
+        string[] ids = new string[modifyConservation.dialogs.Count];
+        for (int i = 0; i < modifyConservation.dialogs.Count; i++)
         {
-			ids[i] = modifyConservation.dialogs[i].dialogId;
-		}
+            ids[i] = modifyConservation.dialogs[i].dialogId;
+        }
 
-		return ids;
+        return ids;
     }
 
-	public string GetNotCreatedId()
+    public string GetNotCreatedId()
     {
-		int id = 0;
-		while(true)
+        int id = 0;
+        while (true)
         {
-			bool exist = false;
-			for(int i = 0; i < modifyConservation.dialogs.Count; i ++)
+            bool exist = false;
+            for (int i = 0; i < modifyConservation.dialogs.Count; i++)
             {
-				if(modifyConservation.dialogs[i].dialogId.Equals("dialog" + (id + 1)))
+                if (modifyConservation.dialogs[i].dialogId.Equals("dialog" + (id + 1)))
                 {
-					id++;
-					exist = true;
-					break;
-				}
+                    id++;
+                    exist = true;
+                    break;
+                }
             }
 
-			if(!exist)
+            if (!exist)
             {
-				break;
-			}
-		}
+                break;
+            }
+        }
 
-		return "dialog" + (id + 1);
+        return "dialog" + (id + 1);
 
-	}
+    }
 
-	private void DeleteDialog(int id)
+    private void DeleteDialog(int id)
     {
-		blocking = true;
+        blocking = true;
 
-		if (id >= 0 && id < modifyConservation.dialogs.Count)
+        if (id >= 0 && id < modifyConservation.dialogs.Count)
         {
-			selectedNode = -1;
-			modifyConservation.dialogs.RemoveAt(id);
-		}
-		SetupNodeData();
-		Repaint();
-	}
+            selectedNode = -1;
+            modifyConservation.dialogs.RemoveAt(id);
+        }
+        SetupNodeData();
+        Repaint();
+    }
+
+
+
+
+
 }
 
 #endif
---
+++ Assets/DialogSystem/Enumerations/DSDialogType.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace DS.Enumerations
+{
+    public enum DSDialogType
+    {
+       SingleChoice,
+       MultipleChoice
+    }
+
+}
